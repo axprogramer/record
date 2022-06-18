5664efa9be5fafbab452b7b4849bab34b05a7228
axprogramer <105089786+axprogramer@users.noreply.github.com>
2022-06-18 07:13:45 +0700

    update

---
+++ asset/js/main.js
@@ -0,0 +1,170 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAOX5I_BB9soXF4yHMp9NCPVk2Z-d3DEPE",
+    authDomain: "teachingrecord-6b575.firebaseapp.com",
+    databaseURL: "https://teachingrecord-6b575-default-rtdb.firebaseio.com",
+    projectId: "teachingrecord-6b575",
+    storageBucket: "teachingrecord-6b575.appspot.com",
+    messagingSenderId: "1097574891233",
+    appId: "1:1097574891233:web:d69ed85c4f4b83daad41a0"
+  };
+  now = new Date();
+  randomNum = '';
+  randomNum += Math.round(Math.random() * 2);
+  randomNum += Math.round(Math.random() * 2);
+  randomNum += now.getTime().toString().slice(-2);
+  document.getElementById("myID").value = randomNum;
+  
+  firebase.initializeApp(firebaseConfig);
+
+  var recordADB = firebase.database().ref('recordA');
+  document.getElementById('body').addEventListener("submit", submitForm);
+
+  function submitForm(e){
+    e.preventDefault();
+    var week = getElementVal('myWeek');
+    var date = getElementVal('myDate');
+    var pay = getElementVal('myPay');
+    var other = getElementVal('myOther');
+    var id = getElementVal('myID');
+    window.location.reload();
+
+    saveData(week,date,pay,other,id);
+
+  }
+const saveData = (week, date,pay,other,id) => {
+    var newData = recordADB.push();
+    newData.set({
+        week: week,
+        date: date,
+        pay: pay,
+        other: other,
+        id: id,
+    });
+};
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
+function selectAllData(){
+  document.getElementById('tbody1').innerHTML="";
+  studentN0=0;
+  firebase.database().ref('recordA').once('value',
+  function(AllRecords){
+    AllRecords.forEach(
+      function(CurrentRecord){
+        var week = CurrentRecord.val().week;
+        var date = CurrentRecord.val().date;
+        var pay = CurrentRecord.val().pay;
+        var other = CurrentRecord.val().other;
+        var id = CurrentRecord.val().id;
+        addItemsToTable(week, date,pay,other,id);
+      }
+    );
+  });
+}
+window.onload = selectAllData;
+var studentN0;
+var stdList = [];
+function addItemsToTable(week,date,pay,other,id){
+  var tbody = document.getElementById('tbody1');
+  var trow = document.createElement('tr');
+  var td0 = document.createElement('td');
+  var td1 = document.createElement('td');
+  var td2 = document.createElement('td');
+  var td3 = document.createElement('td');
+  var td4 = document.createElement('td');
+  var td5 = document.createElement('td');
+  stdList.push([week,date,pay,other,id]);
+  td0.innerHTML = ++studentN0;
+  td1.innerHTML = week;
+  td2.innerHTML = date;
+  td3.innerHTML = pay;
+  td4.innerHTML = id;
+  td5.innerHTML = other;
+
+  trow.appendChild(td0);
+  trow.appendChild(td1);
+  trow.appendChild(td2);
+  trow.appendChild(td3);
+  trow.appendChild(td4);
+  trow.appendChild(td5);
+
+  var ControlDiv = document.createElement("div");
+  ControlDiv.innerHTML = '<button type="button" class="button-6" role="button" data-toggle="modal" data-target="#exampleModalCenter" onclick="Fillbox('+studentN0+')">Edit</button>'
+  trow.appendChild(ControlDiv);
+  tbody.appendChild(trow);
+}
+
+var Mweek = document.getElementById('myWeek');
+var Mdate = document.getElementById('myDate');
+var Mpay = document.getElementById('myPay');
+var Mother = document.getElementById('myOther');
+var Mid = document.getElementById('myID');
+
+var BtnSubmit = document.getElementById('mySubmit');
+var BtnUpdate = document.getElementById('myUpdate');
+var BtnDele = document.getElementById('myDele');
+
+
+function Fillbox(index){
+  if(index==null){
+    BtnSubmit.style.display='inline-block';
+    BtnUpdate.style.display='none';
+    BtnDele.style.display='none';
+  }
+  else{
+    --index;
+    Mweek.value = stdList[index][0];
+    Mdate.value = stdList[index][1];
+    Mpay.value = stdList[index][2];
+    Mother.value = stdList[index][3];
+    Mid.value = stdList[index][4];
+    BtnSubmit.style.display='none';
+    BtnUpdate.style.display='inline-block';
+    BtnDele.style.display='inline-block';
+
+  }
+}
+
+function AddStd(){
+  firebase.database().ref("recordA/"+Mid.value).set(
+    {
+      week: Mweek.value,
+      date: Mdate.value,
+      pay: Mpay.value,
+      id: Mid.value,
+      other: Mother.value,
+    }, 
+  )
+  selectAllData();
+  window.location.reload();
+
+}
+function UpStd(){
+  firebase.database().ref("recordA/"+Mid.value).update(
+    {
+      week: Mweek.value,
+      date: Mdate.value,
+      pay: Mpay.value,
+      other: Mother.value,
+    }, 
+  )
+  selectAllData();
+  window.location.reload();
+
+}
+function DelStd(){
+  firebase.database().ref("recordA/"+Mid.value).remove().then(
+    function(){
+      selectAllData();
+      window.location.reload();
+   
+    }
+  )
+
+
+}
+function DelStdAll(){
+  firebase.database().ref("recordA").remove();
+
+      window.location.reload();
+}
--- asset/js/myReport2.js
+++ asset/js/myReport2.js
@@ -1,199 +1,170 @@
-var studentArray = [];
-/// Push button for data to save
-function saveData(){
-    var week = document.getElementById("myWeek").value;
-    var date = document.getElementById("myDate").value;
-    var pay = document.getElementById("myPay").value;
-    var id = document.getElementById("myID").value;
+const firebaseConfig = {
+    apiKey: "AIzaSyAOX5I_BB9soXF4yHMp9NCPVk2Z-d3DEPE",
+    authDomain: "teachingrecord-6b575.firebaseapp.com",
+    databaseURL: "https://teachingrecord-6b575-default-rtdb.firebaseio.com",
+    projectId: "teachingrecord-6b575",
+    storageBucket: "teachingrecord-6b575.appspot.com",
+    messagingSenderId: "1097574891233",
+    appId: "1:1097574891233:web:d69ed85c4f4b83daad41a0"
+  };
+  now = new Date();
+  randomNum = '';
+  randomNum += Math.round(Math.random() * 2);
+  randomNum += Math.round(Math.random() * 2);
+  randomNum += now.getTime().toString().slice(-2);
+  document.getElementById("myID").value = randomNum;
+  
+  firebase.initializeApp(firebaseConfig);
 
-    var other = document.getElementById("myOther").value;
+  var recordB = firebase.database().ref('recordB');
+  document.getElementById('body').addEventListener("submit", submitForm);
 
-    var dele = document.getElementById("sSinfo").value;
-    var deleMe = document.getElementById("sSinfo").value;
-
-    inputTable(dele,week,date,pay,other,deleMe);
-
-    //Push data to save
-    var sObj = {week:week,date:date,pay:pay,id:id,other:other};
-    studentArray.push(sObj);
-    // Data location Name
-    localStorage.myRpt = JSON.stringify(studentArray);
+  function submitForm(e){
+    e.preventDefault();
+    var week = getElementVal('myWeek');
+    var date = getElementVal('myDate');
+    var pay = getElementVal('myPay');
+    var other = getElementVal('myOther');
+    var id = getElementVal('myID');
     window.location.reload();
 
-}
-now = new Date();
-randomNum = '';
-randomNum += Math.round(Math.random() * 2);
-randomNum += Math.round(Math.random() * 2);
-randomNum += now.getTime().toString().slice(-2);
-document.getElementById("myID").value = randomNum;
+    saveData(week,date,pay,other,id);
 
-function inputTable(dele,week,date,pay,id,other){
-    var table = document.getElementById("sSinfo").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell2 = newRow.insertCell(1);
-    cell3 = newRow.insertCell(2);
-    cell4 = newRow.insertCell(3);
-    cell5 = newRow.insertCell(4);
-    cell6 = newRow.insertCell(5);
-
-     cell1.innerHTML = week;
-     cell2.innerHTML = date;
-     cell3.innerHTML = pay;
-     cell4.innerHTML = id;
-     cell5.innerHTML = other;
-     cell6.innerHTML = 
-     '<button onClick="myDelete('+dele+')" class="button-6" role="button">Delete</button><button onClick="onEdit(this)" class="button-6" role="button">Edit</button>'
-     
-    
-}
-
-//Restore Data back
-function getData(){
-    if (localStorage.myRpt){
-        document.getElementsByName('tbody').innerHTML = "";
-        studentArray = JSON.parse(localStorage.myRpt);
-        for (var i = 0 ; i < studentArray.length; i++){
-            inputTable(
-                i,
-                studentArray[i].week,
-                studentArray[i].date,
-                studentArray[i].pay,
-                studentArray[i].id,
-                studentArray[i].other,
-                )
-        }
-    }
-}
-//Restore Data for report
-function getDataReport2(){
-    if (localStorage.myRpt){
-        document.getElementsByName('tbody').innerHTML = "";
-        studentArray = JSON.parse(localStorage.myRpt);
-        for (var i = 0 ; i < studentArray.length; i++){
-            showData(
-                studentArray[i].week,
-                studentArray[i].date,
-                studentArray[i].pay,
-                studentArray[i].other,
-                )
-        }
-    }
-}
-////Restore Data report for showing only
-function showData(week,date,pay,other){
-    var table = document.getElementById("sSinfo").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell2 = newRow.insertCell(1);
-    cell3 = newRow.insertCell(2);
-    cell4 = newRow.insertCell(3);
-
-     cell1.innerHTML = week;
-     cell2.innerHTML = date;
-     cell3.innerHTML = pay;
-     cell4.innerHTML = other;     
-    
-}
-
-//Delete Data
-function onDelete(dele,week,date,pay,other){
-    document.getElementById("sSinfo").deleteRow(dele+1);
-    studentArray.splice(dele, 1);
-    localStorage.myRpt = JSON.stringify(studentArray);
-    var week = document.getElementById("myWeek").value;
-    var date = document.getElementById("myDate").value;
-    var pay = document.getElementById("myPay").value;
-    var other = document.getElementById("myOther").value;
-    var dele = document.getElementById("sSinfo").value;
-
-
-    selectedRow.cells[0].innerHTML = week;
-    selectedRow.cells[1].innerHTML = date;
-    selectedRow.cells[2].innerHTML = pay;
-    selectedRow.cells[3].innerHTML = other;
-   
-
-    inputTable(dele,week,date,pay,other);
-
-    //Push data to save
-    var sObj = {week:week,date:date,pay:pay,other:other};
-    studentArray.push(sObj);
-    // Data location Name
-    localStorage.myRpt = JSON.stringify(studentArray);
-
-
-}
-
-//Edit Data
-var selectedRow = null
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("myWeek").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("myDate").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("myPay").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("myID").value = selectedRow.cells[3].innerHTML;
-    document.getElementById("myOther").value = selectedRow.cells[4].innerHTML;
-
-} 
-//Update INFO
-function onUpdate(tu){
-    selectedRow = tu.parentElement.parentElement;
-    var week = document.getElementById("myWeek").value;
-    var date = document.getElementById("myDate").value;
-    var pay = document.getElementById("myPay").value;
-    var other = document.getElementById("myOther").value;
-
-    selectedRow.cells[0].innerHTML = week;
-    selectedRow.cells[1].innerHTML = date;
-    selectedRow.cells[2].innerHTML = pay;
-    selectedRow.cells[3].innerHTML = other;
-
-}
-let myRpt = JSON.parse(localStorage.getItem("myRpt"));
-function updateQuantity(myId, week,date,pay,other){
-	for(let product of myRpt){
-		let getId = document.getElementById("myID").value;
-		if(getId == product.id){
-			myId = document.getElementById("myID").value;
-			product.week = week;
-            product.date = date;
-			product.pay = pay;
-			product.other = other;
-		}
-	}
-	localStorage.setItem("myRpt", JSON.stringify(myRpt));
-}
-function updateData(){
-    var week = document.getElementById("myWeek").value;
-    var date = document.getElementById("myDate").value;
-    var pay = document.getElementById("myPay").value;
-    var other = document.getElementById("myOther").value;
-	updateQuantity(1, week,date,pay,other);
-    window.location.reload();
-}
-
-function myDelete(deleMe){
-    document.getElementById("sSinfo").deleteRow(deleMe+1);
-    studentArray.splice(deleMe, 1);
-    localStorage.myRpt = JSON.stringify(studentArray);
-      
-
-}
-function onClear(){
-        localStorage.removeItem('myRpt');
-        window.location.reload();
-
-}
-
-//Enter to sumit Data
-document.onkeypress = keyPress;
-
-function keyPress(e){
-  var x = e || window.event;
-  var key = (x.keyCode || x.which);
-  if(key == 13 || key == 3){
-    saveData();
   }
+const saveData = (week, date,pay,other,id) => {
+    var newData = recordB.push();
+    newData.set({
+        week: week,
+        date: date,
+        pay: pay,
+        other: other,
+        id: id,
+    });
+};
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
+function selectAllData(){
+  document.getElementById('tbody1').innerHTML="";
+  studentN0=0;
+  firebase.database().ref('recordB').once('value',
+  function(AllRecords){
+    AllRecords.forEach(
+      function(CurrentRecord){
+        var week = CurrentRecord.val().week;
+        var date = CurrentRecord.val().date;
+        var pay = CurrentRecord.val().pay;
+        var other = CurrentRecord.val().other;
+        var id = CurrentRecord.val().id;
+        addItemsToTable(week, date,pay,other,id);
+      }
+    );
+  });
+}
+window.onload = selectAllData;
+var studentN0;
+var stdList = [];
+function addItemsToTable(week,date,pay,other,id){
+  var tbody = document.getElementById('tbody1');
+  var trow = document.createElement('tr');
+  var td0 = document.createElement('td');
+  var td1 = document.createElement('td');
+  var td2 = document.createElement('td');
+  var td3 = document.createElement('td');
+  var td4 = document.createElement('td');
+  var td5 = document.createElement('td');
+  stdList.push([week,date,pay,other,id]);
+  td0.innerHTML = ++studentN0;
+  td1.innerHTML = week;
+  td2.innerHTML = date;
+  td3.innerHTML = pay;
+  td4.innerHTML = id;
+  td5.innerHTML = other;
+
+  trow.appendChild(td0);
+  trow.appendChild(td1);
+  trow.appendChild(td2);
+  trow.appendChild(td3);
+  trow.appendChild(td4);
+  trow.appendChild(td5);
+
+  var ControlDiv = document.createElement("div");
+  ControlDiv.innerHTML = '<button type="button" class="button-6" role="button" data-toggle="modal" data-target="#exampleModalCenter" onclick="Fillbox('+studentN0+')">Edit</button>'
+  trow.appendChild(ControlDiv);
+  tbody.appendChild(trow);
+}
+
+var Mweek = document.getElementById('myWeek');
+var Mdate = document.getElementById('myDate');
+var Mpay = document.getElementById('myPay');
+var Mother = document.getElementById('myOther');
+var Mid = document.getElementById('myID');
+
+var BtnSubmit = document.getElementById('mySubmit');
+var BtnUpdate = document.getElementById('myUpdate');
+var BtnDele = document.getElementById('myDele');
+
+
+function Fillbox(index){
+  if(index==null){
+    BtnSubmit.style.display='inline-block';
+    BtnUpdate.style.display='none';
+    BtnDele.style.display='none';
+  }
+  else{
+    --index;
+    Mweek.value = stdList[index][0];
+    Mdate.value = stdList[index][1];
+    Mpay.value = stdList[index][2];
+    Mother.value = stdList[index][3];
+    Mid.value = stdList[index][4];
+    BtnSubmit.style.display='none';
+    BtnUpdate.style.display='inline-block';
+    BtnDele.style.display='inline-block';
+
+  }
+}
+
+function AddStd(){
+  firebase.database().ref("recordB/"+Mid.value).set(
+    {
+      week: Mweek.value,
+      date: Mdate.value,
+      pay: Mpay.value,
+      id: Mid.value,
+      other: Mother.value,
+    }, 
+  )
+  selectAllData();
+  window.location.reload();
+
+}
+function UpStd(){
+  firebase.database().ref("recordB/"+Mid.value).update(
+    {
+      week: Mweek.value,
+      date: Mdate.value,
+      pay: Mpay.value,
+      other: Mother.value,
+    }, 
+  )
+  selectAllData();
+  window.location.reload();
+
+}
+function DelStd(){
+  firebase.database().ref("recordB/"+Mid.value).remove().then(
+    function(){
+      selectAllData();
+      window.location.reload();
+   
+    }
+  )
+
+
+}
+function DelStdAll(){
+  firebase.database().ref("recordB").remove();
+
+      window.location.reload();
 }
